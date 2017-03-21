1f30655ca317848ae4f6f5a6112c250488085556
Barchard <lb185070@ncr.com>
2017-03-21 19:00:15 -0400

    initial project files

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCo1z2M19beBgnl1W1-_GuptApduatb7QY",
+        authDomain: "rock-paper-scissors-6966a.firebaseapp.com",
+        databaseURL: "https://rock-paper-scissors-6966a.firebaseio.com",
+        storageBucket: "rock-paper-scissors-6966a.appspot.com",
+        messagingSenderId: "653657616245"
+    };
+    firebase.initializeApp(config);
+
+    //Initial variables***************************************************************
+    var database = firebase.database();
+
+    //Functions**********************************************************
+    
+    //Event listeners***************************************************************
+    
+    //Startup routine***************************************************************
+    
+});
